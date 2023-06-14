2baca91bc3a1f1c2f0cb3f81c52b8229dab6ca1f
Tanjila13 <136145710+Tanjila13@users.noreply.github.com>
2023-06-14 17:58:22 +0600

    Add files via upload

--- MathclassDemo.java
+++ MathclassDemo.java
@@ -1,26 +1,53 @@
 
-package day7;
+package pkg611basicday1;
 
 
 public class MathclassDemo {
-    public static void main(String[] args){
-    
-        int x = -3;
-        int y = 2;
+    public static void main(String[] args) {
         
-        int max = Math.max(x, y);
-        System.out.println("Max= "+max);
-        
-        System.out.println(Math.min(x, y));
-        
-        int absolute = Math.abs(x);
-        System.out.println("Absolute of x = "+absolute);
-    
-        double power = Math.pow(x, y);
-        System.out.println("x to the power y = "+power);
-        
-       int round = Math.round(6.6f);
+       int x =5;
+       int y = 10;
+       int z = 2;
+       int max,min,absulate,round;
+       double power,pi ;
+               
+       max = Math.max(x,y);
+       System.out.println("Max= "+max);
+       
+       min = Math.min(x,y);
+       System.out.println("Min= "+min);
+       
+       //System.out.println(Math.max(x, y)); find max another way 
+       //System.out.println(Math.min(x, y)); find min another way 
+      //  System.out.println(Math.abs(y)); find absulate another way 
+      //System.out.println(Math.round(5.4));find round another way 
+       //System.out.println(Math.PI);find pi another way 
+       
+       
+       absulate = Math.abs(y);
+       System.out.println("Absulate of y= "+absulate);
+       
+       power = Math.pow(y,z);
+       System.out.println("y power of x= "+power);
+       
+       round = Math.round(5.5f); //convert into integer
        System.out.println("Round = "+round);
+       
+       pi = Math.PI; //convert into integer
+       System.out.println("values of pi = "+pi);
+       
+       System.out.println(Math.E);
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
     }
     
 }
